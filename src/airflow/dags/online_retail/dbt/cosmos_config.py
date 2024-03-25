aa2b04a96dbe9f4efc50b92afd1eb35f14da00e7
RaymondOnn <raymondonn@gmail.com>
2024-03-25 08:36:46 +0800

    fixed formatting issues

--- src/airflow/dags/online_retail/dbt/cosmos_config.py
+++ src/airflow/dags/online_retail/dbt/cosmos_config.py
@@ -7,9 +7,9 @@ from pathlib import Path
 DBT_CONFIG = ProfileConfig(
     profile_name='online_retail',
     target_name='dev',
-    profiles_yml_filepath=Path('/opt/airflow/dags/online_retail/dbt/profiles.yml')
+    profiles_yml_filepath=Path('/opt/airflow/dags/online_retail/dbt/profiles.yml'),
 )
 
-DBT_PROJECT_CONFIG=ProjectConfig(
+DBT_PROJECT_CONFIG = ProjectConfig(
     dbt_project_path='/opt/airflow/dags/online_retail/dbt/',
 )
