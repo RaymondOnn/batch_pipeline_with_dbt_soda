6d1d2b07164977484fbe8f11ca9fd61447ffee10
RaymondOnn <raymondonn@gmail.com>
2024-04-16 13:59:06 +0800

    adding terraform

---
+++ src/terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "online-retail-417320"
+bq_dataset_name = "online_retail"
---
+++ src/terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "project_id" {
+    type = string
+}
+
+variable "region" {
+    type        = string
+    description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+    default     = "asia-southeast1"
+}
+
+variable "storage_class" {
+    type        = string
+    description = "The Storage Class of the new bucket. Ref: https://cloud.google.com/storage/docs/storage-classes"
+    default     = "STANDARD"
+}
+
+variable "bq_dataset_name" {
+    type        = string
+    description = "Dataset in BigQuery where raw data (from Google Cloud Storage) will be loaded."
+    default     = "some_dataset"
+}
